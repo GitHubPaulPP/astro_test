76facb213327c2aa3989c1da20630719a1ab8900
PaulPP <github.paulpp@gmail.com>
2024-03-01 22:11:38 +0100

    update new paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://githubpaulpp.github.io',
-    //base: '/astro_test',
+    site: 'https://GitHubPaulPP.github.io',
+    base: '/astro_test',
 });
 
